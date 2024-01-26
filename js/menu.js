49561e18144a7c9e7a977b17ce57cef09885e0b2
Juanes_1504 <juanestebanalvarezholguin@gmail.com>
2024-01-25 23:37:55 -0500

    solución bug menu

--- js/menu.js
+++ js/menu.js
@@ -9,12 +9,16 @@ function loadContent(file, documento = '') {
     event.preventDefault();
     const contentContainer = document.getElementById("workspace");
     const xhttp = new XMLHttpRequest();
+    // Variable de bandera para controlar si el contenido ya ha sido cargado
+    let contenidoCargado = false;
     xhttp.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
+        if (!contenidoCargado && this.readyState === 4 && this.status === 200) {
             contentContainer.innerHTML = this.responseText;
-            if(!file.includes("listar")){
+            if (!file.includes("listar")) {
                 generarNumeroAleatorio();
             }
+            // Actualiza la variable de bandera
+            contenidoCargado = true;
         }
     };
     xhttp.open("GET", file, true);
